7bc2c2fe1804f04ae72ae0986bc7f0643bac3098
YusufKaya65 <y.yusufkaya6534@gmail.com>
2025-08-20 08:28:18 +0300

    My first commit

---
+++ calculator.js
@@ -0,0 +1,79 @@
+
+function which_operation(){
+  var operation = prompt("Yapmak istediğiniz işlemin giriniz "
+    +" toplama"
+    +" çıkarma" 
+    +" çarpma"
+    +" bölme")
+
+    operation.toLowerCase;
+
+    if (operation == "toplama"){
+      var a = prompt("Sayı giriniz")
+      var b = prompt("Sayı giriniz")
+      addition(a,b);
+    }
+    else if(operation =="çıkarma"){
+      var a = prompt("Sayı giriniz")
+      var b = prompt("Sayı giriniz")
+      subtraction();
+    }
+    else if(operation == "çarpma"){
+      var a = prompt("Sayı giriniz")
+      var b = prompt("Sayı giriniz")
+      multiplication();
+    }
+    else if(operation =="bölme"){
+      var a = prompt("Sayı giriniz")
+      var b = prompt("Sayı giriniz")
+      division();
+    }
+    else{
+      islem();
+    }
+
+}
+
+
+function addition(a,b){
+    var sum = a+b;
+    console.log("Toplam " + sum)
+}
+
+function subtraction(a,b){
+    var sub = a+b;
+    console.log("Çıkarma " + sub)
+}
+
+function multiplication(a,b){
+    var multip = a+b;
+    console.log("Çarpma " + multip)
+}
+
+function division(a,b){
+  var reminder = a%b;
+  var devi= a/b;
+    console.log("Bölüm " + sum
+      +"/nKalan" + devi
+    )
+}
+
+function main(){
+  
+  which_operation();
+  var exit_or_not = prompt("Çıkış yapmak isitiyor musunuz?" +
+    "/n çıkış yapmak için E'ye bas" 
+    +"/devam etmek için bir tuşa bas"
+  )
+  exit_or_not.toLocaleLowerCase;
+  if(exit_or_not != "e"){
+    which_operation();
+  }
+  else{
+    console.log("program exit")
+  }
+
+}
+
+
+main();
